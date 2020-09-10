d3532a96b3d19992e8d88f5b546708656a199982
leon <qwas1049@gmail.com>
2020-09-10 11:45:13 +0800

    feat: dataReciver demo code

--- App.js
+++ App.js
@@ -1,114 +1,217 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, { useState, useEffect, useMemo } from "react";
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
-} from 'react-native';
+  StyleSheet,
+  FlatList,
+  View,
+  ScrollView,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { getMockData } from "./data.js";
 
-const App: () => React$Node = () => {
+const optionEnum = {
+  id: "id",
+  time: "time"
+};
+
+const App = () => {
+  const [deviceData, setDeviceData] = useState([]);
+  const [chosenId, setChosenId] = useState("");
+  const [chosenOption, setChosenOption] = useState(optionEnum.id);
+  const [optionToggle, setOptionToggle] = useState(false);
+  useEffect(() => {
+    /*
+    const fetchData = async () => {
+      return await fetch(
+        "http://167.99.171.123:8080/api/report"
+      );
+    };
+    */
+    //because of the cors policy, I use mock data instead.
+    const data = getMockData();
+    setDeviceData(data.data);
+  }, []);
+  const formatDate = (date) => {
+    const tmpDate = new Date(date);
+    return `${tmpDate.getFullYear()}/${
+      tmpDate.getMonth() + 1
+    }/${tmpDate.getDate()}`;
+  };
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.itemRow}>
+        <View style={styles.item}>
+          <Text>{item.id}</Text>
+        </View>
+        <View style={styles.item}>
+          <Text>{`lat: ${item.gps?.lat}`}</Text>
+          <Text>{`long: ${item.gps?.long}`}</Text>
+        </View>
+        <View style={styles.item}>
+          <Text>{`lat: ${item.battery?.battery1}`}</Text>
+          <Text>{`long: ${item.battery?.battery2}`}</Text>
+        </View>
+        <View style={styles.item}>
+          <Text>{item.date ? formatDate(item.date) : ""}</Text>
+        </View>
+      </View>
+    );
+  };
+
+  const sortedDeviceData = useMemo(() => {
+    const filteredData = chosenId
+      ? deviceData.filter((d) => {
+          return d.id?.includes(chosenId);
+        })
+      : deviceData;
+    let sortedData = [];
+    if (chosenOption === "id") {
+      sortedData = filteredData.sort((a, b) => a.id.localeCompare(b.id));
+    }
+    if (chosenOption === "time") {
+      sortedData = filteredData.sort(
+        (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+      );
+    }
+    return sortedData;
+  }, [deviceData, chosenOption, chosenId]);
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+    <ScrollView style={styles.app} contentContainerStyle={styles.scroller}>
+      <View style={styles.inputPanel}>
+        <Text style={styles.inputTitle}>id:</Text>
+        <TextInput
+          style={styles.input}
+          value={chosenId}
+          onChangeText={(text) => {
+            setChosenId(() => text);
+          }}
+        />
+        <Text style={styles.inputTitle}>orderBy:</Text>
+        <View style={styles.optionPanel}>
+          <TouchableOpacity
+            onPress={() => {
+              setOptionToggle((preOptionToggle) => !preOptionToggle);
+            }}
+          >
+            <Text style={styles.optionBtnText}>{chosenOption}</Text>
+          </TouchableOpacity>
+        </View>
+        {optionToggle && (
+          <>
+            {Object.keys(optionEnum).map((o) => (
+              <TouchableOpacity
+                key={o}
+                style={styles.optionItem}
+                onPress={() => {
+                  setChosenOption(() => o);
+                  setOptionToggle(() => false);
+                }}
+              >
+                <Text style={styles.optionBtnText}>{o}</Text>
+              </TouchableOpacity>
+            ))}
+          </>
+        )}
+      </View>
+      <View style={{ width: "100%", alignItems: "center" }}>
+        <View style={[styles.itemRow, { borderWidth: 0, height: 30 }]}>
+          <View style={styles.item}>
+            <Text>id</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+          <View style={styles.item}>
+            <Text>gps</Text>
+          </View>
+          <View style={styles.item}>
+            <Text>battery</Text>
+          </View>
+          <View style={styles.item}>
+            <Text>date</Text>
+          </View>
+        </View>
+        <FlatList
+          style={{ width: "99%" }}
+          data={sortedDeviceData}
+          renderItem={renderItem}
+          keyExtractor={(_, index) => `${index}`}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  app: {
+    flex: 1,
+    marginVertical: 20
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  scroller: {
+    alignItems: "center"
   },
-  body: {
-    backgroundColor: Colors.white,
+  inputPanel: {
+    height: 35,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 15
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  inputTitle: {
+    marginHorizontal: 10,
+    fontSize: 16
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  input: {
+    width: 80,
+    height: "100%",
+    borderWidth: 1
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  itemRow: {
+    flexDirection: "row",
+    width: "100%",
+    height: 80,
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center"
   },
-  highlight: {
-    fontWeight: '700',
+  item: {
+    flex: 1,
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center"
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  searchBtn: {
+    width: 80,
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 10
   },
+  searchBtnText: {
+    color: "#fff"
+  },
+  optionPanel: {
+    width: 80,
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    backgroundColor: "rgba(0,0,0,0.1)",
+    borderColor: "rgba(0,0,0,0.2)"
+  },
+  optionBtnText: {
+    color: "#000"
+  },
+  optionItem: {
+    marginLeft: 5,
+    width: 40,
+    height: 35,
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 255, 255, 0.3)",
+    borderColor: "rgba(0,0,0,0.2)"
+  }
 });
 
 export default App;
---
+++ data.js
@@ -0,0 +1,343 @@
+export const getMockData = () => {
+    return {
+      data: [
+        {
+          id: "AB002",
+          gps: { lat: "121.3054019", long: "25.0338953" },
+          battery: { battery1: 40, battery2: 100 },
+          motion: { x: "12", y: "3", z: "52" },
+          date: "2020-05-29T23:12:11",
+          date_uploaded: "2020-08-26T09:39:03"
+        },
+        {
+          id: "AB002",
+          gps: { lat: "121.3054019", long: "25.0338953" },
+          battery: { battery1: 40, battery2: 100 },
+          motion: { x: "12", y: "3", z: "52" },
+          date: "2020-05-29T23:12:11",
+          date_uploaded: "2020-07-24T02:37:54"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.666482", long: "121.421402" },
+          battery: { battery1: 50, battery2: 55 },
+          motion: { x: "53", y: "-52", z: "1056" },
+          date: "2020-05-12T12:11:22",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.666482", long: "121.421402" },
+          battery: { battery1: 50, battery2: 55 },
+          motion: { x: "53", y: "-52", z: "1056" },
+          date: "2020-05-12T04:11:22",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.666482", long: "121.421402" },
+          battery: { battery1: 50, battery2: 55 },
+          motion: { x: "53", y: "-52", z: "1056" },
+          date: "2020-05-11T20:11:22",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.666482", long: "121.421402" },
+          battery: { battery1: 50, battery2: 55 },
+          motion: { x: "53", y: "-52", z: "1056" },
+          date: "2020-05-11T12:11:22",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.666482", long: "121.421402" },
+          battery: { battery1: 45, battery2: 55 },
+          motion: { x: "53", y: "-52", z: "1056" },
+          date: "2020-05-11T04:11:22",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.666482", long: "121.421402" },
+          battery: { battery1: 45, battery2: 55 },
+          motion: { x: "53", y: "-52", z: "1056" },
+          date: "2020-05-10T20:11:22",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.666482", long: "121.421402" },
+          battery: { battery1: 45, battery2: 55 },
+          motion: { x: "53", y: "-52", z: "1056" },
+          date: "2020-05-10T16:11:22",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.666482", long: "121.421402" },
+          battery: { battery1: 45, battery2: 55 },
+          motion: { x: "53", y: "-52", z: "1056" },
+          date: "2020-05-10T14:11:22",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.666482", long: "121.421402" },
+          battery: { battery1: 45, battery2: 55 },
+          motion: { x: "53", y: "-52", z: "1056" },
+          date: "2020-05-10T13:11:22",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.666482", long: "121.421402" },
+          battery: { battery1: 45, battery2: 55 },
+          motion: { x: "53", y: "-52", z: "1056" },
+          date: "2020-05-10T12:41:22",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.666720", long: "121.422828" },
+          battery: { battery1: 40, battery2: 55 },
+          motion: { x: "223", y: "-36", z: "1057" },
+          date: "2020-05-10T12:11:22",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.683829", long: "121.409247" },
+          battery: { battery1: 40, battery2: 55 },
+          motion: { x: "122", y: "-25", z: "1011" },
+          date: "2020-05-10T11:49:44",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.684317", long: "121.409959" },
+          battery: { battery1: 40, battery2: 55 },
+          motion: { x: "289", y: "-76", z: "1024" },
+          date: "2020-05-10T11:28:09",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.680518", long: "121.397732" },
+          battery: { battery1: 40, battery2: 60 },
+          motion: { x: "84", y: "368", z: "1290" },
+          date: "2020-05-10T11:09:44",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.679860", long: "121.396540" },
+          battery: { battery1: 40, battery2: 60 },
+          motion: { x: "471", y: "-43", z: "1034" },
+          date: "2020-05-10T10:10:50",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.680015", long: "121.396720" },
+          battery: { battery1: 40, battery2: 60 },
+          motion: { x: "214", y: "-23", z: "1074" },
+          date: "2020-05-10T09:39:48",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.674152", long: "121.422350" },
+          battery: { battery1: 40, battery2: 60 },
+          motion: { x: "84", y: "368", z: "1290" },
+          date: "2020-05-10T09:31:20",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB012",
+          gps: { lat: "23.666375", long: "121.421352" },
+          battery: { battery1: 40, battery2: 60 },
+          motion: { x: "54", y: "-54", z: "1044" },
+          date: "2020-05-10T09:11:11",
+          date_uploaded: "2020-06-09T02:38:15"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666475", long: "121.421374" },
+          battery: { battery1: 50, battery2: 50 },
+          motion: { x: "67", y: "-5", z: "1210" },
+          date: "2020-05-14T04:11:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666475", long: "121.421374" },
+          battery: { battery1: 50, battery2: 50 },
+          motion: { x: "67", y: "-5", z: "1210" },
+          date: "2020-05-13T20:11:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666475", long: "121.421374" },
+          battery: { battery1: 50, battery2: 50 },
+          motion: { x: "67", y: "-5", z: "1210" },
+          date: "2020-05-13T12:11:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666475", long: "121.421374" },
+          battery: { battery1: 50, battery2: 50 },
+          motion: { x: "67", y: "-5", z: "1210" },
+          date: "2020-05-13T04:11:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666475", long: "121.421374" },
+          battery: { battery1: 50, battery2: 50 },
+          motion: { x: "67", y: "-5", z: "1210" },
+          date: "2020-05-12T20:11:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666475", long: "121.421374" },
+          battery: { battery1: 45, battery2: 50 },
+          motion: { x: "67", y: "-5", z: "1210" },
+          date: "2020-05-12T04:11:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666475", long: "121.421374" },
+          battery: { battery1: 45, battery2: 50 },
+          motion: { x: "67", y: "-5", z: "1210" },
+          date: "2020-05-11T20:11:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666475", long: "121.421374" },
+          battery: { battery1: 45, battery2: 50 },
+          motion: { x: "67", y: "-5", z: "1210" },
+          date: "2020-05-11T12:11:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666475", long: "121.421374" },
+          battery: { battery1: 40, battery2: 50 },
+          motion: { x: "67", y: "-5", z: "1210" },
+          date: "2020-05-11T04:11:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666475", long: "121.421374" },
+          battery: { battery1: 40, battery2: 50 },
+          motion: { x: "67", y: "-5", z: "1210" },
+          date: "2020-05-10T20:11:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666475", long: "121.421374" },
+          battery: { battery1: 40, battery2: 50 },
+          motion: { x: "67", y: "-5", z: "1210" },
+          date: "2020-05-10T16:11:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666475", long: "121.421374" },
+          battery: { battery1: 40, battery2: 50 },
+          motion: { x: "67", y: "-5", z: "1210" },
+          date: "2020-05-10T14:11:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666475", long: "121.421374" },
+          battery: { battery1: 40, battery2: 50 },
+          motion: { x: "67", y: "-5", z: "1210" },
+          date: "2020-05-10T13:11:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666475", long: "121.421374" },
+          battery: { battery1: 40, battery2: 50 },
+          motion: { x: "67", y: "-5", z: "1210" },
+          date: "2020-05-10T12:41:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666720", long: "121.422828" },
+          battery: { battery1: 35, battery2: 50 },
+          motion: { x: "323", y: "9", z: "1211" },
+          date: "2020-05-10T12:11:22",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.683829", long: "121.409247" },
+          battery: { battery1: 35, battery2: 50 },
+          motion: { x: "171", y: "18", z: "1158" },
+          date: "2020-05-10T11:49:44",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.684317", long: "121.409959" },
+          battery: { battery1: 35, battery2: 50 },
+          motion: { x: "423", y: "-25", z: "1173" },
+          date: "2020-05-10T11:28:09",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.680518", long: "121.397732" },
+          battery: { battery1: 35, battery2: 55 },
+          motion: { x: "113", y: "343", z: "1479" },
+          date: "2020-05-10T11:09:42",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.679860", long: "121.396540" },
+          battery: { battery1: 35, battery2: 55 },
+          motion: { x: "698", y: "3", z: "1185" },
+          date: "2020-05-10T10:11:10",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.680015", long: "121.396720" },
+          battery: { battery1: 35, battery2: 55 },
+          motion: { x: "310", y: "19", z: "1231" },
+          date: "2020-05-10T09:40:40",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.677208", long: "121.420400" },
+          battery: { battery1: 35, battery2: 55 },
+          motion: { x: "113", y: "343", z: "1479" },
+          date: "2020-05-10T09:32:21",
+          date_uploaded: "2020-06-09T02:38:07"
+        },
+        {
+          id: "AB014",
+          gps: { lat: "23.666269", long: "121.421399" },
+          battery: { battery1: 35, battery2: 55 },
+          motion: { x: "68", y: "-7", z: "1196" },
+          date: "2020-05-10T09:15:55",
+          date_uploaded: "2020-06-09T02:38:07"
+        }
+      ]
+    };
+  };
+  
